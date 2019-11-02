b758515f3ed452e46d803d5f57aad3b8a7b49565
dheodhks00 <54461215+dheodhks00@users.noreply.github.com>
2019-11-02 12:40:27 +0900

    Add files via upload

---
+++ 1102_DML_01.sql
@@ -0,0 +1,57 @@
+create database study_db;
+
+show databases;
+
+use study_db;
+
+create table book(
+	bookid int primary key auto_increment,
+    bookname varchar(20) not null,
+    publisher varchar(40) not null, 
+    price int
+);
+
+insert into book(bookname, publisher, price) values("축구의 역사", "굿 미디어", 25000);
+
+
+
+
+select *
+from book
+where price < 20000;
+
+select *
+from book
+where price between 10000 and 20000;
+
+select *
+from book
+where price >= 10000 and price <= 20000;
+
+select *
+from book
+where publisher in (' 굿스포츠', '대한미디어');
+
+select *
+from book
+where publisher not in (' 굿스포츠', '대한미디어');
+
+select bookname, publisher
+from book
+where bookname like '축구의 역사';
+
+select bookname, publisher
+from book
+where bookname like '%축구%';
+
+select *
+from customer
+where name like "%김%";
+
+select *
+from book
+order by bookname;
+
+select sum(saleprice) as total 
+from orders; 
+
